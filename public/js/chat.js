7d86cdaf1cbe0e2768dfcf276b63ffaf5731b386
Erick Buitrago <erickbuitrago2014@gmail.com>
2022-08-31 23:08:16 -0500

    modelo para los mensajes y sockets cliente-servidor

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,6 +2,13 @@
 let usuario = null;
 let socket = null;
 
+// Referencias HTML
+const txtUid = document.querySelector('#txtUid');
+const txtMensaje = document.querySelector('#txtMensaje');
+const ulUsuarios = document.querySelector('#ulUsuarios');
+const ulMensajes = document.querySelector('#ulMensajes');
+const btnSalir = document.querySelector('#btnSalir');
+
 // Validar el token del localStorage
 const validarJWT = async () => {
 
@@ -22,12 +29,56 @@ const validarJWT = async () => {
 }
 
 const conectarSocket = async () => {
-    const socket = io({
+    socket = io({
         'extraHeaders': {
             'x-token': localStorage.getItem('token')
         }
     });
-    
+
+    socket.on('connect', () => {
+        console.log('socket conectado');
+    });
+
+    socket.on('disconnect', () => {
+        console.log('socket desconectado');
+    });
+
+    socket.on('recibir-mensajes', (payload) => {
+        console.log(payload);
+    });
+
+    socket.on('usuarios-activos', agregarUsuarios);
+
+    socket.on('mensaje-privado', () => {
+        //TODO
+    });
+}
+
+txtMensaje.addEventListener('keyup', (e) => {
+    const mensaje = e.target.value;
+    const uid = '';
+
+    if (e.keyCode !== 13) return;
+    if (mensaje.length === 0) return;
+
+    // Es recomendable siempre enviar un objeto, así sea una sola propiedad
+    socket.emit('enviar-mensaje', { mensaje, uid });
+    e.target.value = '';
+});
+
+const agregarUsuarios = (usuarios = []) => {
+    let usersHtml = '';
+    usuarios.forEach(({ nombre, uid }) => {
+        usersHtml += `
+            <li>
+                <p>
+                    <h5 class="text-success">${nombre}</h5>
+                    <span class="fs-6 text-muted">${uid}</span>
+                </p>
+            </li>
+        `
+    });
+    ulUsuarios.innerHTML = usersHtml;
 }
 
 const main = async () => {
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,14 +1,37 @@
 const { Socket } = require("socket.io");
 const { comprobarJWT } = require("../helpers");
+const { ChatMensajes } = require("../models");
 
-// Solo para un entorno de desarrollo => new Socket
-const socketController = async (socket = new Socket) => {
+const chatMensajes = new ChatMensajes();
+
+/**
+ * @param {*} socket 
+ * @param {*} io Representa todo el servidor de sockets
+ * @returns undefined
+ */
+const socketController = async (socket = new Socket, io) => { // Solo para un entorno de desarrollo => new Socket
+
     const token = socket.handshake.headers['x-token'];
-    let usuario = await comprobarJWT(token);
+    const usuario = await comprobarJWT(token);
     if (!usuario) {
         return socket.disconnect();
     }
-    console.log('se conecto:', usuario.nombre);
+
+    // Agregar el usuario conectado
+    chatMensajes.conectarUsuario(usuario);
+    io.emit('usuarios-activos', chatMensajes.usuariosArr)
+
+    // Limpiar cuando alguien se desconecta
+    socket.on('disconnect', () => {
+        chatMensajes.desconectarUsuario(usuario._id); // revisar porque se envía el id de esta forma
+        io.emit('usuarios-activos', chatMensajes.usuariosArr);
+    });
+
+    socket.on('enviar-mensaje', ({ uid, mensaje }) => {
+        chatMensajes.enviarMensaje(usuario._id, usuario.nombre, mensaje);
+        io.emit('recibir-mensajes', chatMensajes.ultimos10);
+    });
+
 }
 
 module.exports = {
